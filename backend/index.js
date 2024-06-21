85716e5cb37014e4c9c3c06ae512a0ea846dff2a
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-06-21 13:45:05 +0700

    ✨

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}))
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
+
+app.listen(process.env.APP_PORT, () => {
+    console.log('server running...');
+})
