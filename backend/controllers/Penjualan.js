a1587b97a12a17e0c842bbac42d39f5b66bcfe7a
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-07-03 16:05:21 +0700

    create penjualan

---
+++ backend/controllers/Penjualan.js
@@ -0,0 +1,21 @@
+import PenjualanModel from '../models/PenjualanModel';
+
+export const getPenjualan = async (req, res) => {
+
+}
+
+export const getPenjualanById = async (req, res) => {
+    
+}
+
+export const createPenjualan = async (req, res) => {
+    
+}
+
+export const updatePenjualan = async (req, res) => {
+    
+}
+
+export const deletePenjualan = async (req, res) => {
+    
+}
