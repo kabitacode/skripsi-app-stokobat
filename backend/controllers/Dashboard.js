2ac13feb9f33cae8ef607fb3cab88941a515aa56
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-07-05 14:22:50 +0700

    add dashboard

--- backend/controllers/Dashboard.js
+++ backend/controllers/Dashboard.js
@@ -1,13 +1,46 @@
 import ObatModel from "../models/ObatModel.js";
-
+import BatchObatModel from "../models/BatchObatModel.js";
+import { Op } from 'sequelize';
 
 export const getData = async (req, res) => {
     try {
-        const obat = await ObatModel.findByPk(req.userId);
-        let stok = obat
+        // Mendapatkan semua obat
+        const allObat = await ObatModel.findAll({
+            include: [{
+                model: BatchObatModel
+            }]
+        });
 
-        console.log("result =>>", stok);
+        // Menghitung total stok, obat kedaluwarsa, dan obat mendekati kedaluwarsa
+        let totalStok = 0;
+        let totalKadaluarsa = 0;
+        let totalMendekatiKadaluarsa = 0;
+
+        const currentDate = new Date();
+        const oneMonthLater = new Date();
+        oneMonthLater.setMonth(oneMonthLater.getMonth() + 1);
+
+        allObat.forEach(obat => {
+            totalStok += obat.stok;
+            obat.batch_obats.forEach(batch => {
+                if (batch.tanggal_produksi > obat.tanggal_kadaluarsa) {
+                    totalKadaluarsa++;
+                } else if (batch.tanggal_produksi <= oneMonthLater && batch.tanggal_produksi >= currentDate) {
+                    totalMendekatiKadaluarsa++;
+                }
+            });
+        });
+
+        res.status(200).json({
+            status: 200,
+            message: "success",
+            data: {
+                totalStok,
+                totalKadaluarsa,
+                totalMendekatiKadaluarsa
+            }
+        });
     } catch (error) {
-        
+        res.status(500).json({ message: error.message });
     }
 }
