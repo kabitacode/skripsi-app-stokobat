333b2709146bc9a9ce8351f1f8912830a95cfdaa
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-07-05 08:44:18 +0700

    change updatebatch route

--- backend/routes/BatchRoute.js
+++ backend/routes/BatchRoute.js
@@ -14,7 +14,7 @@ import { verifyUser } from "../middleware/AuthUser.js";
 router.get('/api/batch', verifyUser, getBatch);
 router.get('/api/batch/:id', verifyUser, getBatchById);
 router.post('/api/batch', verifyUser, createBatch);
-router.put('/api/batch', verifyUser, updateBatch);
+router.patch('/api/batch/:id', verifyUser, updateBatch);
 router.delete('/api/batch/:id', verifyUser, deleteBatch);
 
 
