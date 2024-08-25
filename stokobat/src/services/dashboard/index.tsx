7606cd42d0be743952a23784e2d22e12e5b2d3d2
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-08-25 16:16:22 +0700

    add api dashboard penjualan by bulan

--- stokobat/src/services/dashboard/index.tsx
+++ stokobat/src/services/dashboard/index.tsx
@@ -13,3 +13,17 @@ export const fetchDashboard = async (token: string) => {
       throw error;
     }
   };
+
+  export const fetchDashboardPenjualan = async (token: string, bulan: any, tahun: any) => {
+    try {
+      const response = await api.get(`/dashboard/penjualan?bulan=${bulan}&tahun=${tahun}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
