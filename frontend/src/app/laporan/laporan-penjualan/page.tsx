dc248316d87040cab290613bf25d0bf9c6fb061e
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-13 14:08:48 +0700

    ✨ add filter penjualan

--- frontend/src/app/laporan/laporan-penjualan/page.tsx
+++ frontend/src/app/laporan/laporan-penjualan/page.tsx
@@ -6,7 +6,7 @@ import DashboardLayout from "../../dashboard/layout";
 import Link from 'next/link';
 import { Add, ArrowDropDownCircle, Delete, Download, Edit } from "@mui/icons-material";
 import { CustomButton, ButtonCustom } from "@/components";
-import { fetchDataLaporan, fetchLaporanKadaluarsa, fetchLaporanMendekatiKadaluarsa, fetchLaporanObat, fetchLaporanPenjualan, fetchObat, fetchObatDelete } from '@/services';
+import { fetchDataLaporan, fetchFilterPenjualan, fetchFilterPenjualanByEmptyStok, fetchLaporanKadaluarsa, fetchLaporanMendekatiKadaluarsa, fetchLaporanObat, fetchLaporanPenjualan, fetchObat, fetchObatDelete } from '@/services';
 import useStore, { User } from '@/store/useStore'
 import { Table, TablePagination, TableHead, TableRow, TableCell, TableBody, CircularProgress, Button, Alert, AlertTitle, TextField, Menu, Fade, IconButton } from '@mui/material';
 import { toast } from 'react-hot-toast';
@@ -37,11 +37,12 @@ const Page: React.FC = () => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [searchQuery, setSearchQuery] = useState<string>('');
-    const [isLaporanKey, setIsLaporanKey] = useState<string>('Laporan');
-    const [isLaporan, setIsLaporan] = useState<string>('Laporan Obat');
+
     const [start_date, setStartDate] = useState<Dayjs | null>(dayjs());
     const [end_date, setEndDate] = useState<Dayjs | null>(dayjs());
-    
+    const [status, setStatus] = React.useState('');
+    const [statusStok, setStatusStok] = React.useState('');
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -56,12 +57,43 @@ const Page: React.FC = () => {
 
         try {
             const apiData = await fetchDataLaporan(user?.token);
-            setData(apiData.data.laporan);
-            setDataMendekatiKadaluarsa(apiData.data.mendekatiKadaluarsa);
-            setDataKadaluarsa(apiData.data.kadaluarsa);
             setDataPenjualan(apiData.data.penjualan);
 
             setLoading(false);
+        } catch (error: any) {
+            toast.error(error.response?.data?.message || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const getFilter = async () => {
+        setLoading(true)
+        if (!user || !user.token) return;
+        try {
+            const tanggalMulai = start_date?.format('YYYY-MM-DD')
+            const tanggalSelesai = end_date?.format('YYYY-MM-DD')
+            const response = await fetchFilterPenjualan(user?.token, tanggalMulai, tanggalSelesai)
+            setDataPenjualan(response.data);
+
+
+            toast.success(response.message || "Success!");
+        } catch (error: any) {
+            toast.error(error.response?.data?.message || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const getFilterByEmptyStok = async (param: any) => {
+        setLoading(true)
+        if (!user || !user.token) return;
+        try {
+            const response = await fetchFilterPenjualanByEmptyStok(user?.token)
+            setDataPenjualan(response.data);
+            console.log(response.data);
+
+            toast.success(response.message || "Success!");
         } catch (error: any) {
             toast.error(error.response?.data?.message || error.message);
         } finally {
@@ -86,18 +118,8 @@ const Page: React.FC = () => {
         if (!user || !user.token) return;
 
         try {
-            let apiData;
+            let apiData = await fetchLaporanPenjualan(user?.token);
 
-            if (isLaporanKey == "Penjualan") {
-                apiData = await fetchLaporanPenjualan(user?.token);
-            } else if (isLaporanKey == "Mendekati") {
-                apiData = await fetchLaporanMendekatiKadaluarsa(user?.token);
-            } else if (isLaporanKey == "Kadaluarsa") {
-                apiData = await fetchLaporanKadaluarsa(user?.token);
-            } else {
-                apiData = await fetchLaporanObat(user?.token);
-            }
-            
             toast.success(apiData.message || "Data berhasil Didownload!");
             fetchData();
             setLoading(false);
@@ -117,6 +139,11 @@ const Page: React.FC = () => {
         return dayjs(dateString).format('DD MMMM YYYY');
     };
 
+    const handleChangeStok = (event: SelectChangeEvent) => {
+        setStatusStok(event.target.value);
+        getFilterByEmptyStok(event.target.value)
+    };
+
     return (
         <DashboardLayout>
             {
@@ -126,38 +153,6 @@ const Page: React.FC = () => {
             <div className="flex mt-4 mr-5 ml-5 mb-5 justify-between">
                 <div className='flex flex-row items-center justify-center'>
                     <h1 className="text-2xl font-semibold">Laporan Penjualan</h1>
-                    {/* <div className="">
-                        <IconButton
-                            id="demo-customized-button"
-                            onClick={handleClick}>
-                            <ArrowDropDownCircle />
-                        </IconButton>
-
-                        <Menu
-                            id="fade-menu"
-                            MenuListProps={{
-                                'aria-labelledby': 'fade-button',
-                            }}
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            TransitionComponent={Fade}
-                        >
-                            {
-                                dataMenuLaporan.map((item) => {
-                                    return (
-                                        <MenuItem onClick={() => {
-                                            handleClose()
-                                            setIsLaporan(item.title)
-                                            setIsLaporanKey(item.key)
-                                        }}>{item.title}</MenuItem>
-                                    )
-                                })
-                            }
-
-
-                        </Menu>
-                    </div> */}
                 </div>
 
                 <div className="">
@@ -189,72 +184,72 @@ const Page: React.FC = () => {
                             />
                         </LocalizationProvider>
                     </div>
-                    <Button variant="contained" sx={{ height: 40 }} onClick={() => {}}>Filter</Button>
+                    <Button variant="contained" sx={{ height: 40 }} onClick={() => getFilter()}>Filter</Button>
                 </div>
             </div>
 
             <div className='ml-5 mb-5'>
-                    <h4 className="text-lg font-semibold mb-3">Filter By Stok</h4>
-                    <div className="mr-3">
-                        <FormControl sx={{ minWidth: 140 }} size="small">
-                            <InputLabel id="demo-select-small-label">Status Stok</InputLabel>
-                            <Select
-                                labelId="demo-select-small-label"
-                                id="demo-select-small"
-                                value={""}
-                                label="Age"
-                                onChange={() => {}}
-                            >
-                                <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={"true"}>Stok Habis</MenuItem>
-                                <MenuItem value={""}>Semua Stok</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
+                <h4 className="text-lg font-semibold mb-3">Filter By Stok</h4>
+                <div className="mr-3">
+                    <FormControl sx={{ minWidth: 140 }} size="small">
+                        <InputLabel id="demo-select-small-label">Status Stok</InputLabel>
+                        <Select
+                            labelId="demo-select-small-label"
+                            id="demo-select-small"
+                            value={statusStok}
+                            label="Age"
+                            onChange={handleChangeStok}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={"true"}>Stok Habis</MenuItem>
+                            <MenuItem value={""}>Semua Stok</MenuItem>
+                        </Select>
+                    </FormControl>
                 </div>
+            </div>
 
-            
 
-                    <div className='mx-5'>
-                        <Table>
-                            <TableHead className='bg-blue-700'>
-                                <TableRow>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>No</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Nama Obat</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Stok</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Harga Obat (pcs)</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Jumlah</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Total Harga</TableCell>
-                                    <TableCell sx={{ color: 'white', fontWeight: '600' }}>Tanggal Transaksi</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {dataPenjualan.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
-                                    <TableRow key={item.id}>
-                                        <TableCell>{index + 1}</TableCell>
-                                        <TableCell>{item.nama_obat}</TableCell>
-                                        <TableCell>{item.stok}</TableCell>
-                                        <TableCell>{item.harga_obat}</TableCell>
-                                        <TableCell>{item.jumlah}</TableCell>
-                                        <TableCell>{item.total_harga}</TableCell>
-                                        <TableCell>{formattedDate(item.tanggal_transaksi)}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25]}
-                            component="div"
-                            count={data.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                        />
-                    </div>
-           
+
+            <div className='mx-5'>
+                <Table>
+                    <TableHead className='bg-blue-700'>
+                        <TableRow>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>No</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Nama Obat</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Stok</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Harga Obat (pcs)</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Jumlah</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Total Harga</TableCell>
+                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Tanggal Transaksi</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {dataPenjualan.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
+                            <TableRow key={item.id}>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>{item.nama_obat}</TableCell>
+                                <TableCell>{item.stok}</TableCell>
+                                <TableCell>{item.harga_obat}</TableCell>
+                                <TableCell>{item.jumlah}</TableCell>
+                                <TableCell>{item.total_harga}</TableCell>
+                                <TableCell>{formattedDate(item.tanggal_transaksi)}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </div>
+
         </DashboardLayout>
     )
 }
