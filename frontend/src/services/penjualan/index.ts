50ab35e6ad2a83156be81c73f8e5aa6d14f6c05a
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-13 09:06:10 +0700

    ✨ add services fetch api penjualan filtered

--- frontend/src/services/penjualan/index.ts
+++ frontend/src/services/penjualan/index.ts
@@ -87,3 +87,17 @@ export const fetchPenjualan = async (token: string) => {
       throw error;
     }
   };
+
+  export const fetchPenjualanFilter = async (token: string, start_date: any, end_date: any) => {
+    try {
+      const response = await api.get(`/penjualan/filter?start_date=${start_date}&end_date=${end_date}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
